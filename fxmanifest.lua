8c7a5ff1c4838f8c1655bb4e5272d37a9360108e
DeceptDevv <130248921+DeceptDevv@users.noreply.github.com>
2023-04-08 12:59:20 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 
 game 'gta5'
 author 'ZAD4Y'
-description 'https://github.com/westonon/weston-npc'
+description 'https://github.com/DeceptDevv/Deceptdevv-npc'
 version '1.0.1'
 
 client_script 'main.lua'
